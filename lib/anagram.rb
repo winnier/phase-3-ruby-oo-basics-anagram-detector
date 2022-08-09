10c7f34f3467142376c8a1d764d4548f6b70f9b1
Winnie Mendoza <wrm956@gmail.com>
2022-08-09 09:36:33 -0400

    commit tree f4f44fb444461fba710d79a500445a8b4e010678

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.select { |n|  n.chars.sort == @word.chars.sort }
+    end
+end
